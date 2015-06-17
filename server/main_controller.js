b566ca43f9d8a9ab6fd7701a6d57db9b3cfb6a62
Tarun <tarun1188@gmail.com>
2015-06-17 16:32:28 +0530

    Commenting mongodb beacause heroku doesn't support it in free apps

--- server/main_controller.js
+++ server/main_controller.js
@@ -2,7 +2,7 @@ function MainController(server){
 
 	var io = require('socket.io')(server);
 	var socket_controller = require('./socket_controller.js')(io);
-	var db_helper = require('./db_helper.js');
+	// var db_helper = require('./db_helper.js');
 }
 
 module.exports = MainController;
